a75f80610905723178b09c139fa07270582324c1
Alejandro Licona <al.licona@duocuc.cl>
2024-10-30 02:46:20 -0300

    Se ha implementado conexion al crud desde formulario de mascotas, se ha implementado coe en el tab de perros y gatos ademas se ha implementado plugin native en el tab de veterinario

--- src/app/pages/centro-asistencia/gatos/gatos.page.ts
+++ src/app/pages/centro-asistencia/gatos/gatos.page.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-gatos',
   templateUrl: './gatos.page.html',
   styleUrls: ['./gatos.page.scss'],
 })
-export class GatosPage implements OnInit {
+export class GatosPage {
+  sectionOpen: string | null = null;
+  exampleOpen: { [key: string]: boolean } = {};
+  detailOpen: { [key: string]: { [key: string]: boolean } } = {};
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  toggleSection(section: string) {
+    this.sectionOpen = this.sectionOpen === section ? null : section;
   }
 
+  toggleExample(example: string) {
+    this.exampleOpen[example] = !this.exampleOpen[example];
+  }
+
+  isExampleOpen(example: string): boolean {
+    return this.exampleOpen[example] || false;
+  }
+
+  toggleDetail(detail: string, section: string) {
+    if (!this.detailOpen[section]) {
+      this.detailOpen[section] = {};
+    }
+    this.detailOpen[section][detail] = !this.detailOpen[section][detail];
+  }
+
+  isDetailOpen(detail: string, section: string): boolean {
+    return this.detailOpen[section]?.[detail] || false;
+  }
 }
--- src/app/pages/centro-asistencia/veterinario/veterinario.page.ts
+++ src/app/pages/centro-asistencia/veterinario/veterinario.page.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-veterinario',
   templateUrl: './veterinario.page.html',
   styleUrls: ['./veterinario.page.scss'],
 })
-export class VeterinarioPage implements OnInit {
+export class VeterinarioPage {
+
+  map!:L.Map
+
+  latitude: number | null = null;
+  longitude: number | null = null;
 
   constructor() { }
+  
 
-  ngOnInit() {
+  ionViewDidEnter(){
+      this.getCurrentPosition()
+  
   }
+    
+  
 
+  async getCurrentPosition() {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+  
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+  
+      console.log(`Latitud: ${this.latitude}, Longitud: ${this.longitude}`);
+  
+      // Verifica si el mapa ya está inicializado y, si es así, remuévelo
+      if (this.map) {
+        this.map.remove();
+      }
+  
+      // Inicializa el mapa en el contenedor 'mapId'
+      this.map = L.map('mapId').setView([this.latitude, this.longitude], 6);
+      
+      // Agrega la capa de OpenStreetMap
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(this.map);
+  
+      // Agrega un marcador en la ubicación actual
+      const markPoint = L.marker([this.latitude, this.longitude]);
+      this.map.addLayer(markPoint);
+  
+    } catch (error) {
+      console.error('Error obteniendo la ubicación', error);
+    }
+    // try {
+    //   const position = await Geolocation.getCurrentPosition();
+      
+    //   this.latitude = position.coords.latitude;
+    //   this.longitude = position.coords.longitude;
+
+    //   console.log(`Latitud: ${this.latitude}, Longitud: ${this.longitude}`);
+    //   this.map= L.map('mapId').setView([this.latitude!,this.longitude!],6);
+    //   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //   }).addTo(this.map);
+    //   const markPoint = L.marker([this.latitude!,this.longitude!])
+    //   this.map.addLayer(markPoint)
+    // } catch (error) {
+    //   console.error('Error obteniendo la ubicación', error);
+    // }
+  }
 }
